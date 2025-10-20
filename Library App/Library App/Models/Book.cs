1dd0a7f0aea58ff0183d0b88476a81ddb836feb2
ZaZenZe <desayan2014@gmail.com>
2025-10-20 18:39:19 +0200

    Add project files.

---
+++ Library App/Library App/Models/Book.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Library_App.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [Range(0, 3000)]
+    public int Year { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    [RegularExpression(@"^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$", 
+        ErrorMessage = "Invalid ISBN format")]
+    public string Isbn { get; set; } = string.Empty;
+
+    [Required]
+    public int AuthorId { get; set; }
+
+    public int? PublisherId { get; set; }
+
+    // Navigation properties
+    public Author? Author { get; set; }
+    public Publisher? Publisher { get; set; }
+}
